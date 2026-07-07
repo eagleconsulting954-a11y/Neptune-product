b9f355866fb920c6622387cd748c1f90448d3410
eagleconsulting954-a11y <eagleconsulting954@gmail.com>
2026-07-07 11:17:10 -0400

    Reposition navigation for operator product

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -6,14 +6,15 @@ export function SiteHeader() {
       <div className="container header-inner">
         <Link className="brand" href="/">
           <span className="brand-mark">◈</span>
-          <span>NEPTUNE<small>Maritime Intelligence</small></span>
+          <span>NEPTUNE<small>Vessel Command CRM</small></span>
         </Link>
         <nav className="nav-links">
-          <Link href="/#platform">Platform</Link>
-          <Link href="/#lead-magnet">Playbook</Link>
+          <Link href="/#command">Command</Link>
+          <Link href="/#departments">Departments</Link>
+          <Link href="/#workflows">Workflows</Link>
           <Link href="/pricing">Pricing</Link>
           <Link href="/login">Login</Link>
-          <Link className="btn gold" href="/#lead-magnet">Get the playbook</Link>
+          <Link className="btn gold" href="/login">Open dashboard</Link>
         </nav>
       </div>
     </header>
